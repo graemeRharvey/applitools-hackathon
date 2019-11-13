158d25b4f7a78072ba1b42c9a1026d7e18a4bfe2
Graeme Harvey <graeme@plangrid.com>
2019-11-13 10:21:52 -0500

    Check in of initial layout

---
+++ applitools.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    appName: 'applitools-hackaton',
+    apiKey: 'rnR3EGJIIgMOw4eCIqnm102vIhpLBNykBiU2101vxV3YW8g110',
+    showLogs: true,
+    batchName: 'testBatch'
+  }
---
+++ cypress/integration/TraditionalTests.js
@@ -0,0 +1,47 @@
+const selectors = {};
+
+  
+describe("Login Page", () => {
+    before(() => {});
+  
+    beforeEach(() => {});
+  
+    it("Contains all elements", () => {});
+
+    it("Cannot login with no user or password", () => {});
+
+    it("Cannot login with valid user no password", () => {});
+
+    it("Cannot login with valid user no password", () => {});
+
+    it("Can login with valid user and password", () => {});
+});
+
+describe("Recent Transactions", () => {
+    // To skip login, visit: https://demo.applitools.com/hackathonApp.html OR https://demo.applitools.com/hackathonAppV2.html
+    before(() => {});
+  
+    beforeEach(() => {});
+  
+    it("Can sort ascending", () => {});
+});
+
+describe("Compare Expenses", () => {
+    before(() => {});
+  
+    beforeEach(() => {});
+  
+    it("Renders chart correctly", () => {});
+
+    it("Can show data for next year", () => {});
+});
+
+describe("Advertisements", () => {
+    before(() => {
+        // Visit: /hackathon.html?showAd=true OR /hackathonV2.html?showAd=true
+    });
+  
+    beforeEach(() => {});
+  
+    it("Renders two ads after login", () => {});
+});
