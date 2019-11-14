ecfdb52a1eabed00273315bd5f2a11e0583ac145
Graeme Harvey <graeme@plangrid.com>
2019-11-13 23:32:33 -0500

    Add chart visual tests

---
+++ cypress/integration/visualAITests/compare-expenses-visual-spec.js
@@ -0,0 +1,33 @@
+/// <reference types="cypress" />
+
+const selectors = {
+    addDatasetButton: "button#addDataset",
+    chart: "canvas"
+  }
+  
+  describe("Compare Expenses Chart", () => {
+    before(() => {
+      cy.visit("/hackathonChart.html");
+      cy.eyesOpen({
+        testName: 'Compare expenses chart',
+      });
+    });
+
+    after(() => {
+        cy.eyesClose();
+    })
+
+    it("adds valid 2019 data", () => {
+      cy.eyesCheckWindow({
+        sizeMode: 'selector',
+        selector: selectors.chart
+      });
+  
+      cy.get(selectors.addDatasetButton).click();
+
+      cy.eyesCheckWindow({
+        sizeMode: 'selector',
+        selector: selectors.chart
+      });
+    });
+  });
