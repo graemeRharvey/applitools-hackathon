c27a8ecf40686dcb40bc872fc45fd3857c13a8d4
Brendan Drew <brendan.drew@plangrid.com>
2019-11-13 23:26:01 -0500

    Refactor Recent Transaction Tests

--- cypress/integration/traditionalTests/recent-transactions-spec.js
+++ cypress/integration/traditionalTests/recent-transactions-spec.js
@@ -13,10 +13,6 @@ const headersIndecies = [];
 
 const startingData = [];
 
-function getElementHtml(element) {
-  return cy.wrap(element).invoke("html");
-}
-
 describe("Recent Transactions", () => {
   before(() => {
     cy.visit("/hackathonApp.html");
@@ -34,23 +30,38 @@ describe("Recent Transactions", () => {
     cy.get(selectors.tableHeader("amount")).click();
 
     let amounts = [];
-    let sortedData = [];
-    // There's a lot of code here that would be easily cleaned up if we had access
-    // to the app source code to add some useful ids.
-    // For example, the amount fields could have an attribute added to make them
-    // easier to locate. It would become something simpler like cy.get(selectors.amountField).
+    let updatedData = [];
+    /* There's a lot of code here that would be easily cleaned up if we had access
+     to the app source code to add some useful ids.
+     For example, the amount fields could have an attribute added to make them
+     easier to locate. This whole section would become something simpler like:
+
+     cy.get(selectors.amountField).each(field => {
+        cy.wrap(field).invoke("text").each(amount => {
+          amounts.push(parseAmountToFloat(text));
+        });
+     });
+    */
     cy.get(selectors.tableRow).each(row => {
       const amountIndex = headersIndecies.find(h => h.name === "amount").index;
       cy.wrap(row).find("td").eq(amountIndex).find("span").invoke("text").then(text => {
-        text = text.replace("USD", "").replace(" ", "").replace(",", "");
+        amounts.push(parseAmountToFloat(text));
+      });
 
-        amounts.push(parseFloat(text));
-      })
-
-      getElementHtml(row).then(html => {sortedData.push(html)});
+      getElementHtml(row).then(html => {updatedData.push(html)});
     }).then(() => {
       expect(amounts).to.be.sorted({descending: false});
-      expect(sortedData.sort()).to.deep.equal(startingData.sort());
+      expect(updatedData.sort()).to.deep.equal(startingData.sort());
     });
   });
 });
+
+function getElementHtml(element) {
+  return cy.wrap(element).invoke("html");
+}
+
+function parseAmountToFloat(stringAmount) {
+  stringAmount = stringAmount.replace("USD", "").replace(" ", "").replace(",", "");
+
+  return parseFloat(stringAmount);
+}
