bcbafbed9639a6dea69664216d5f92854e97d719
Graeme Harvey <graeme@plangrid.com>
2019-11-13 21:53:42 -0500

    Add VisualAi login tests

--- applitools.config.js
+++ applitools.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-    appName: 'applitools-hackaton',
+    appName: 'applitools-hackathon',
     apiKey: 'rnR3EGJIIgMOw4eCIqnm102vIhpLBNykBiU2101vxV3YW8g110',
     showLogs: true,
-    batchName: 'Hackathon'
+    batchName: 'Hackathon',
+    browser: { width: 800, height: 600 }
   }
---
+++ cypress/integration/visualAITests/login-visual-spec.js
@@ -0,0 +1,107 @@
+/// <reference types="cypress" />
+
+const selectors = {
+    header: 'h4.auth-header',
+    alertWarning: 'div.alert-warning',
+    DivFormGroup: 'div.form-group',
+    InputUsername: '#username',
+    InputPassword: '#password',
+    BtnLogin: '#log-in',
+    DivLoggedInUser: 'div.logged-user-w',
+    DivLogo: 'div.logo-w',
+    DivUsernameLogo: 'div.os-icon-user-male-circle',
+    DivPasswordLogo: 'div.os-icon-fingerprint',
+    InputRemember: 'input.form-check-input',
+    LabelRemember: 'label.form-check-label',
+    ImgTwitter: 'img[src$="twitter.png"]',
+    ImgFacebook: 'img[src$="facebook.png"]',
+    ImgLinkedIn: 'img[src$="linkedin.png"]'
+};
+
+const validUsername = 'testuser';
+const validPassword = 'password';
+
+  
+describe("Login Page", () => {
+
+    beforeEach(() => {
+        cy.visit("/hackathon.html");
+    });
+  
+    it("Contains all elements", () => {
+        cy.eyesOpen({
+            testName: 'Login Page contains all elements',
+        });
+
+        cy.eyesCheckWindow('Login page');
+
+        cy.eyesClose();
+    });
+
+    it("Cannot login with no user or password", () => {
+        cy.eyesOpen({
+            testName: 'Login Page no user or password warning',
+        });
+        
+        cy.login('', '');
+        
+        cy.eyesCheckWindow({
+            sizeMode: 'selector',
+            selector: selectors.alertWarning
+        });
+
+        cy.eyesClose();
+    });
+
+    it("Cannot login with valid user no password", () => {
+        cy.eyesOpen({
+            testName: 'Login Page valid user no password warning',
+        });
+        
+        cy.login(validUsername, '');
+        
+        cy.eyesCheckWindow({
+            sizeMode: 'selector',
+            selector: selectors.alertWarning
+        });
+
+        cy.eyesClose();
+    });
+
+    it("Cannot login with no user valid password", () => {
+        cy.eyesOpen({
+            testName: 'Login Page no user valid password warning',
+        });
+        
+        cy.login('', validPassword);
+        
+        cy.eyesCheckWindow({
+            sizeMode: 'selector',
+            selector: selectors.alertWarning
+        });
+
+        cy.eyesClose();
+    });
+
+    it("Can login with valid user and password", () => {
+        cy.eyesOpen({
+            testName: 'Login Page can login with valid user and password',
+        });
+
+        cy.login(validUsername, validPassword);
+
+        cy.eyesCheckWindow('Logged in');
+
+        cy.eyesClose();
+    });
+});
+
+Cypress.Commands.add('login', (username, password) => {
+    if (username){
+        cy.get(selectors.InputUsername).type(username);
+    }
+    if (password) {
+        cy.get(selectors.InputPassword).type(password);
+    }
+    cy.get(selectors.BtnLogin).click();
+})
